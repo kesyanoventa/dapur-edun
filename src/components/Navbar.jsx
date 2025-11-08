a77e4aaf4a48300c1bc36ec5c42a00ebe452b3fb
hellwize <vanpendidikan09@gmail.com>
2025-11-08 19:32:24 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <h1 className="logo">Dapur Edun</h1>
+        <Link to="/" className="logo">
+          Dapur Edun
+        </Link>
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link 
+              to="/" 
+              className={location.pathname === "/" ? "active" : ""}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/about" 
+              className={location.pathname === "/about" ? "active" : ""}
+            >
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/products" 
+              className={location.pathname === "/products" ? "active" : ""}
+            >
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/contact" 
+              className={location.pathname === "/contact" ? "active" : ""}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
